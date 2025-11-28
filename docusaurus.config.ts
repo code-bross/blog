101a8bd507fecad1c8bdf3b6a836d505424d064a
kis-way <way@koreainvestment.com>
2025-11-28 16:13:47 +0900

    - update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://code-bross.github.io/blog',
+  url: 'https://code-bross.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/blog',
